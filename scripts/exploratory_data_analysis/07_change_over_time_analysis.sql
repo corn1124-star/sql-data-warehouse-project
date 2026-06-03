e6862e4f4c76d33efdda1da5b3d48b230db11122
corn1124-star <thanhphan0101204@gmail.com>
2026-06-03 18:02:00 +0700

    Create 07_change_over_time_analysis.sql

---
+++ scripts/exploratory_data_analysis/07_change_over_time_analysis.sql
@@ -0,0 +1,29 @@
+SELECT
+	YEAR(order_date) AS order_year,
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date)
+ORDER BY YEAR(order_date)
+
+SELECT
+	DATETRUNC(MONTH,order_date) AS order_year,
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(MONTH,order_date)
+ORDER BY DATETRUNC(MONTH,order_date)
+
+SELECT
+	FORMAT(order_date, 'yyyy-MMM') AS order_year,
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY FORMAT(order_date, 'yyyy-MMM')
+ORDER BY FORMAT(order_date, 'yyyy-MMM')
