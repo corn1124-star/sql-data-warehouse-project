17d6cb111e536655de93677ff757176f8bbc6225
corn1124-star <thanhphan0101204@gmail.com>
2026-06-03 17:54:32 +0700

    Create 04_measures _exploration.sql

---
+++ scripts/exploratory_data_analysis/04_measures _exploration.sql
@@ -0,0 +1,36 @@
+-- Find the Total Sales
+SELECT SUM(sales_amount) AS total_sale FROM gold.fact_sales
+
+-- Find how many items are sold
+SELECT SUM(quantity) AS total_quantity FROM gold.fact_sales
+
+-- Find the average selling price
+SELECT AVG(price) AS avg_price FROM gold.fact_sales
+
+-- Find the Total number of Orders
+SELECT COUNT(order_number) AS total_order FROM gold.fact_sales
+SELECT COUNT(DISTINCT order_number) AS total_order FROM gold.fact_sales
+
+-- Find the Total number of products
+SELECT COUNT(product_name) AS total_products FROM gold.dim_products
+SELECT COUNT(DISTINCT product_name) AS total_products FROM gold.dim_products
+
+-- Find the Total number of customers
+SELECT COUNT(customer_key) AS total_customers FROM gold.dim_customers
+
+-- Find the Total number of customers that has placed an order
+SELECT COUNT(DISTINCT customer_key) AS total_customers FROM gold.dim_customers
+
+-- Generate a Report that shows all key metrics of the business
+
+SELECT 'Total Sales' AS measure_name, SUM(sales_amount) AS measure_value FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Quantity' , SUM(quantity) FROM gold.fact_sales
+UNION ALL
+SELECT 'Average Price', AVG(price) FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Nr. Orders', COUNT(DISTINCT order_number) FROM gold.fact_sales
+UNION ALL
+SELECT 'Total Nr. Products', COUNT(product_name) FROM gold.dim_products
+UNION ALL
+SELECT 'Total Nr. Customers', COUNT(customer_key) FROM gold.dim_customers
